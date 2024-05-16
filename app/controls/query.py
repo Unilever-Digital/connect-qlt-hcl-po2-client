860118d3582a62df9a60c24a0cab4a2735e8b464
Dat <dat.lemindast@gmail.com>
2024-05-16 10:12:17 +0700

    update

--- app/controls/query.py
+++ app/controls/query.py
@@ -18,18 +18,17 @@ def queryOptimizationCheckweigher(table):
         DAY(DateTime) AS day,
         MONTH(DateTime) AS month,
         YEAR(DateTime) AS year,
-        CASE
-            WHEN DATEPART(hh, DateTime) < 6 THEN 1
-            WHEN DATEPART(hh, DateTime) < 14 THEN 2
-            ELSE 3
-        END AS shift,
+        Shift AS shift,
 		FGsCode AS sku,
 		Line As line,
+        Weight_Under AS under,
+        Weight_Target AS target,
+        Weight_Over AS over,
         COUNT(*) AS count,
         SUM(CASE WHEN Status = 'Good' THEN 1 ELSE 0 END) AS countGood,
         SUM(CASE WHEN Status = 'Not Good' THEN 1 ELSE 0 END) AS countNotgood
-    FROM Table_ResultCap
-    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), Line, FGsCode,
+    FROM {table}
+    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), Line, FGsCode, Weight_Under, Weight_Target, Weight_Over
             CASE
                 WHEN DATEPART(hh, DateTime) < 6 THEN 1
                 WHEN DATEPART(hh, DateTime) < 14 THEN 2
@@ -49,9 +48,12 @@ def queryOptimizationCheckweigher(table):
             "shift": row[4],
             "sku":  row[5],
             "line": row[6],
-            "count": row[7],
-            "countPass": row[8],
-            "countNotgood": row[9],
+            "under": row[7],
+            "target": row[8],
+            "over": row[9],
+            "count": row[10],
+            "countPass": row[11],
+            "countNotgood": row[12],
         }
         data_insert.append(new_row)
     print(data_insert)
@@ -80,13 +82,14 @@ def queryOptimizationImageFail(table):
             WHEN DATEPART(hh, DateTime) < 14 THEN 2
             ELSE 3
         END AS shift,
-		SKUID AS sku,
+		FGsCode AS sku,
 		Line As line,
+        BarcodeTarget AS target,
+        BarcodeCurrent AS current,
         COUNT(*) AS count,
-        SUM(CASE WHEN Status = 'Good' THEN 1 ELSE 0 END) AS countGood,
-        SUM(CASE WHEN Status = 'WrongCode' THEN 1 ELSE 0 END) AS countWrongCode
-    FROM Table_ResultCarton
-    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), Line, SKUID,
+        COUNT(ImageFail) AS countImageFail
+    FROM {table}
+    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), Line, FGsCode, BarcodeTarget, BarcodeCurrent
             CASE
                 WHEN DATEPART(hh, DateTime) < 6 THEN 1
                 WHEN DATEPART(hh, DateTime) < 14 THEN 2
@@ -106,10 +109,73 @@ def queryOptimizationImageFail(table):
             "shift": row[4],
             "sku":  row[5],
             "line": row[6],
-            "count": row[7],
-            "countGood": row[8],
-            "countWrongCode": row[9],
+            "target": row[7],
+            "current": row[8],
+            "count": row[9],
+            "countImageFail": row[10],
         }
+        data_insert.append(new_row)
+
+    collection.insert_many(data_insert)
+    connection.close()
+
+def queryOptimizationOperating(table):
+    """
+    counter bottles server
+    """
+    connection = connectToSqlServer('DESKTOP-M7H8BIL', 'U-CheckDate-Barcode')
+    cursor = connection.cursor()
+    collection = ensure_collection_exists("U-CheckDate-Barcode-Po2", table)
+
+    result = collection.delete_many({})
+    print(result)
+
+    startdate = datetime.datetime(2023, 1, 1, 0, 0, 0)
+    # Define the pipeline string using f-strings for cleaner formatting
+    pipeline = f"""
+    SELECT CONVERT(date, DateTime) AS date,
+        DAY(DateTime) AS day,
+        MONTH(DateTime) AS month,
+        YEAR(DateTime) AS year,
+        CASE
+            WHEN DATEPART(hh, DateTime) < 6 THEN 1
+            WHEN DATEPART(hh, DateTime) < 14 THEN 2
+            ELSE 3
+        END AS shift,
+        FGsCode AS sku,
+        COUNT(*) AS count,
+        SUM(CASE WHEN Status = 'Good' THEN 1 ELSE 0 END) AS countPass,
+        SUM(CASE WHEN Status = 'NotGood' THEN 1 ELSE 0 END) AS countReject
+    FROM Table_ResultCounterBottles
+    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), FGsCode,
+        CASE
+            WHEN DATEPART(hh, DateTime) < 6 THEN 1
+            WHEN DATEPART(hh, DateTime) < 14 THEN 2
+            ELSE 3
+        END
+    ORDER BY date, shift;
+    """
+    cursor.execute(pipeline)
+    group_data = cursor.fetchall()
+
+    data_insert = []
+    for row in group_data:
+        new_row = {
+            "date": row[0],
+            "day": row[1],
+            "month": row[2],
+            "year": row[3],
+            "shift": row[4],
+            "sku":  row[5],
+            "count": row[6],
+            "countGood": row[7],
+            "countNotGood": row[8],
+        }
+        # for key, value in new_row.items():
+        # if key == "date":
+        # new_row[key] = dt.strftime(value, "%Y-%m-%d")
+        # else:
+        # new_row[key] = value if value != float('nan') else ""
         data_insert.append(new_row)
 
     collection.insert_many(data_insert)
@@ -142,13 +208,8 @@ def queryOptimizationProduct(table):
         COUNT(*) AS count,
         SUM(CASE WHEN Status = 'Good' THEN 1 ELSE 0 END) AS countPass,
         SUM(CASE WHEN Status = 'NotGood' THEN 1 ELSE 0 END) AS countReject
-    FROM Table_ResultCounterBottles
-    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), FGsCode,
-        CASE
-            WHEN DATEPART(hh, DateTime) < 6 THEN 1
-            WHEN DATEPART(hh, DateTime) < 14 THEN 2
-            ELSE 3
-        END
+    FROM {table}
+    GROUP BY SKUID
     ORDER BY date, shift;
     """
     cursor.execute(pipeline)
@@ -193,14 +254,14 @@ def queryOptimizationResultCarton(table):
         MONTH(DateTime) AS month,
         YEAR(DateTime) AS year,
         Shift AS shift,
-		FGsCode AS sku,
+		FgsCode AS sku,
         ProductName AS productName,
 		Line As line,
         COUNT(*) AS count,
         SUM(CASE WHEN Status = 'Good' THEN 1 ELSE 0 END) AS countGood,
         SUM(CASE WHEN Status = 'Not Good' THEN 1 ELSE 0 END) AS countNotgood
-    FROM Table_ResultCap
-    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), Line, FGsCode,
+    FROM {table}
+    GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), Line, FGsCode, ProductName
         CASE
             WHEN DATEPART(hh, DateTime) < 6 THEN 1
             WHEN DATEPART(hh, DateTime) < 14 THEN 2
@@ -219,10 +280,11 @@ def queryOptimizationResultCarton(table):
             "year": row[3],
             "shift": row[4],
             "sku":  row[5],
-            "line": row[6],
-            "count": row[7],
-            "countPass": row[8],
-            "countNotgood": row[9],
+            "name": row[6],
+            "line": row[7],
+            "count": row[8],
+            "countGood": row[9],
+            "countNotgood": row[10],
         }
         data_insert.append(new_row)
     print(data_insert)
@@ -252,7 +314,7 @@ def queryOptimizationResultDataman(table):
         COUNT(*) AS count,
         SUM(CASE WHEN Status = 'Good' THEN 1 ELSE 0 END) AS countGood,
         SUM(CASE WHEN Status = 'Not Good' THEN 1 ELSE 0 END) AS countNotgood
-    FROM Table_ResultCap
+    FROM {table}
     GROUP BY CONVERT(date, DateTime), DAY(DateTime), MONTH(DateTime), YEAR(DateTime), Line, FGsCode,
         CASE
             WHEN DATEPART(hh, DateTime) < 6 THEN 1
@@ -272,10 +334,11 @@ def queryOptimizationResultDataman(table):
             "year": row[3],
             "shift": row[4],
             "sku":  row[5],
-            "line": row[6],
-            "count": row[7],
-            "countPass": row[8],
-            "countNotgood": row[9],
+            "name": row[6].
+            "line": row[7],
+            "count": row[8],
+            "countPass": row[9],
+            "countNotgood": row[10],
         }
         data_insert.append(new_row)
     print(data_insert)
@@ -283,11 +346,10 @@ def queryOptimizationResultDataman(table):
     collection.insert_many(data_insert)
     connection.close()
     
-    
-
 def querySqlServer():
-    queryOptimizationCheckweigher("Table_Checkweigher")
-    queryOptimizationImageFail("Table_ImageFail")
-    queryOptimizationProduct("Table_Product")
-    queryOptimizationResultCarton("Table_ResultCarton")
+    queryOptimizationCheckweigher("Table_Checkweigher") ## done
+    queryOptimizationImageFail("Table_ImageFail") ## done
+    #queryOptimizationOperating("Table_Operating")
+    #queryOptimizationProduct("Table_Product")
+    queryOptimizationResultCarton("Table_ResultCarton") ## done
     queryOptimizationResultDataman("Table_ResultDataman")
